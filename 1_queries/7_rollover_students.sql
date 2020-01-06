b8049df1f10ccea94f3579908908b6c7c857021c
AliceMathews <alice_mathews@hotmail.co.uk>
2020-01-06 22:59:02 +0000

    using join queries

--- 1_queries/7_rollover_students.sql
+++ 1_queries/7_rollover_students.sql
@@ -1,5 +1,5 @@
 SELECT students.name AS student_name, students.start_date AS students_start_date, cohorts.name AS cohort_name, cohorts.start_date AS cohort_start_date
 FROM students 
-JOIN cohorts ON cohorts.id = cohort_id\
+JOIN cohorts ON cohorts.id = cohort_id
 WHERE students.start_date != cohorts.start_date
 ORDER BY cohorts.name;
