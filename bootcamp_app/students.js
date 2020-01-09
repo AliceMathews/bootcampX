d8134144418ac676f8d34176b0de644c409a20f4
AliceMathews <alice_mathews@hotmail.co.uk>
2020-01-09 01:37:48 +0000

    edited code to protect against sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -11,15 +11,16 @@ pool.connect();
 
 let cohortName = process.argv.slice(2)[0];
 let limit = process.argv.slice(2)[1];
-
-
-pool.query(`
+let queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${cohortName}%'
-LIMIT ${limit};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`
+
+
+pool.query(queryString, [`%${cohortName}%`, limit])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -10,16 +10,17 @@ const pool = new Pool({
 pool.connect(); 
 
 let cohortName = process.argv.slice(2)[0];
-
-pool.query(`
+let queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM cohorts
 JOIN students ON cohorts.id = cohort_id
 JOIN assistance_requests ON students.id = student_id
 JOIN teachers ON teachers.id = teacher_id 
-WHERE cohorts.name LIKE '${cohortName}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teachers.name;
-`)
+`
+
+pool.query(queryString, [`${cohortName}%`])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
